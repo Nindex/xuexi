0ebefd6884292f1905d13030cb2aa8fb165d28e7
杨仁义 <2275337077@qq.com>
2026-02-04 01:28:07 +0800

    Initial commit

---
+++ vue3-X/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- html 结构 -->
+ <div class="app">
+    <h1>你好啊</h1>
+ </div>
+</template>
+
+<script lang="ts">
+ //脚本
+ export default {
+    name: 'App',
+ }
+ 
+</script>
+ 
+<style >
+    /* 样式 */
+    .app {
+        box-shadow: 0 0 10px 10px rgba(0, 0, 0, 0.1);
+        background-color: rgb(18, 75, 182);
+        padding: 20px;
+        border-radius: 8px;
+    }
+</style>
