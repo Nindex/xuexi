f01767631381f3c5974e6fbaa320b4d4f968f864
杨仁义 <2275337077@qq.com>
2026-03-02 16:17:40 +0800

    增加了几个按钮

--- vue3-X/src/components/Prseon.vue
+++ vue3-X/src/components/Prseon.vue
@@ -3,20 +3,39 @@
   <div class="perseon">
     <h1>姓名：{{ name }}</h1>
     <p>年龄：{{ age }}</p>
-
+    <button @click="changeAge">减少年龄</button>
+    <button @click="changeAge1">增加年龄</button>
+    <button @click="changeAge2">设置年龄</button>
+    <button @click="changeAge3">清空年龄</button>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue'
 
 export default {
   name: 'Perseon',
   setup() {
     // 数据
-      let name: '张三'
-      let age: 18
-      let TEL: '13800000000'
-    return {name,age,TEL}
+      let name: string = '张三'
+      let age = ref<number>(18)
+      // 方法
+      function changeAge() {
+        age.value--
+      }
+      function changeAge1() {
+        age.value++
+      }
+      // 方法
+      function changeAge2() {
+        age.value = 20
+      }
+      // 方法
+      function changeAge3() {
+        age.value = 0
+      }
+      let TEL: string = '13800000000'
+    return {name,age,changeAge,changeAge1,changeAge2,changeAge3,TEL}  
   }
 }
 </script>
